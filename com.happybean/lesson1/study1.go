73a460401088a1ab366aa5f78b6b79f0c20fcb57
happy-bean <1358876203@qq.com>
2018-08-14 16:27:49 +0800

    Initial commit

---
+++ com.happybean/lesson1/study1.go
@@ -0,0 +1,14 @@
+package lesson1
+
+//Go 标记
+//Go 程序可以由多个标记组成，可以是关键字，标识符，常量，字符串，符号。如以下 GO 语句由 6 个标记组成：
+
+//fmt.Println("Hello, World!")
+//6 个标记是(每行一个)：
+
+//fmt
+//.
+// Println
+//(
+// "Hello, World!"
+//)
